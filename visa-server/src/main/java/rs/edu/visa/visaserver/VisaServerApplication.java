3b3b152a4180e240b4381b7649a0d8d9427a8dd0
Pavle Papuga <ppapuga@execom.eu>
2018-04-15 14:22:07 +0200

    server - initial commit

---
+++ visa-server/src/main/java/rs/edu/visa/visaserver/VisaServerApplication.java
@@ -0,0 +1,12 @@
+package rs.edu.visa.visaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaServerApplication.class, args);
+	}
+}
---
+++ visa-server/src/test/java/rs/edu/visa/visaserver/VisaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package rs.edu.visa.visaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VisaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
