e08d743de4251dd7111bf06af33ce05688146b4f
Pavle Papuga <ppapuga@execom.eu>
2018-04-19 21:20:59 +0200

    client side - initial commit - 2nd attempt

--- visa-client/src/App.tsx
+++ visa-client/src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import './App.css';
-import BeerList from './BeerList';
 
 import logo from './logo.svg';
 
@@ -12,7 +11,9 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <BeerList/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
       </div>
     );
   }
